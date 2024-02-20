612536261eafdad82a19e7809b2a74aa1b772792
Alexey <alexkysin@gmail.com>
2024-02-20 23:35:04 +0700

    My first job=) rev.2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,40 +6,50 @@ public class Main {
         double sum = 0;
         int quantity = 0;
 
-        Scanner scanner = new Scanner(System.in);
         while (true) {
+            Scanner scanner = new Scanner(System.in);
             System.out.println("На скольких человек необходимо разделить счёт?");
-            quantity = scanner.nextInt();
-            if (quantity < 2) {
-                System.out.println("Неверное значение");
+
+            if (scanner.hasNextInt()) {
+                quantity = scanner.nextInt();
+                if (quantity < 2) {
+                    System.out.println("Неверное значение");
+                } else {
+                    break;
+                }
             } else {
-                break;
+                System.out.println("Неверное значение");
             }
         }
+
         ArrayList<Product> productsList = new ArrayList<>();
         while (true) {
             Scanner sc = new Scanner(System.in);
             System.out.println("Введите название товара");
             String name = sc.nextLine();
-            if (name.equalsIgnoreCase("Завершить")) {
-
-                for (int i = 0; i < productsList.size(); i++) {
-                    Product element = productsList.get(i);
-                    System.out.println(element.name + " " + element.price);
-                    sum += element.price;
-                }
-                String rub = Format.formatRub(sum / quantity);
-                System.out.printf("Итого с человека: %.2f" + rub, sum / quantity);
-                break;
-
+            if (name.trim().isEmpty()) {
+                System.out.println("Что-нибудь еще, кроме пустоты?");
             } else {
-                System.out.println("Введите цену");
-                Double price = sc.nextDouble();
-                Product newProduct = new Product(name, price);
-                productsList.add(newProduct);
-                System.out.println("Добавленные товары: ");
-                for (Product k : productsList) {
-                    System.out.println(k.name);
+                if (name.equalsIgnoreCase("Завершить")) {
+
+                    for (int i = 0; i < productsList.size(); i++) {
+                        Product element = productsList.get(i);
+                        System.out.println(element.name + " " + element.price);
+                        sum += element.price;
+                    }
+                    String rub = Format.formatRub(sum / quantity);
+                    System.out.printf("Итого с человека: %.2f" + rub, sum / quantity);
+                    break;
+
+                } else {
+                    System.out.println("Введите цену");
+                    double price = sc.nextDouble();
+                    Product newProduct = new Product(name, price);
+                    productsList.add(newProduct);
+                    System.out.println("Добавленные товары: ");
+                    for (Product k : productsList) {
+                        System.out.println(k.name);
+                    }
                 }
             }
         }
